894f1e6b9852ec29de2ee23849d60bd6d9ee0ec1
abhishek <abhishekbhati1993@gmail.com>
2019-09-17 13:47:39 +0530

    update  file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {SecurityComponent} from './security/security.component';
+import { DownloadComponent } from './download/download.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'home',component:IndexComponent},
+  {path:'security',component:SecurityComponent},
+  {path:'download',component:DownloadComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
